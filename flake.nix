c53fa118c8a75ab40dddc31166a9169118c5c92d
Alexander Braverman Masis <abraverm@redhat.com>
2024-05-10 18:24:42 -0400

    Refactor from poetry2nix to simple buildPythonPackage

--- flake.nix
+++ flake.nix
@@ -3,23 +3,17 @@
   inputs  = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, flake-utils }:
   flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
+        config.allowBroken = true;
       };
 
-      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
-      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) defaultPoetryOverrides;
 
       devEnv = let
         name = "jirasync";
@@ -48,26 +42,37 @@
           export JIRASYNC_CONFIG="~/.jirasync.conf"
         '';
       }).env;
+
+      jira2markdown = with pkgs.python3Packages; buildPythonPackage rec {
+        pname = "jira2markdown";
+        version = "0.3.6";
+        src = fetchPypi {
+          inherit pname version;
+          hash = "sha256-sBSq//Rr5lQ5M5Ew2EE+HxdVjctmBRx31RnjCjME4Z8=";
+        };
+        format = "pyproject";
+        nativeBuildInputs = [poetry-core];
+        dependencies = [ pyparsing ];
+      };
     in 
     {
       packages = {
-        jirasync = mkPoetryApplication {
-          projectDir = self;
-          overrides = defaultPoetryOverrides.extend
-            (self: super: {
-              sphinxcontrib-jquery = super.sphinxcontrib-jquery.overridePythonAttrs
-                (
-                  old: {
-                    buildInputs = (old.buildInputs or [ ]) ++ [ super.sphinx ];
-                  }
-                );
-              jira2markdown = super.jira2markdown.overridePythonAttrs
-                (
-                  old: {
-                    buildInputs = (old.buildInputs or [ ]) ++ [ super.poetry ];
-                  }
-                );
-            });
+        jirasync =  with pkgs.python3Packages; buildPythonPackage  rec {
+          pname = "jirasync";
+          src = ./.;
+          format = "pyproject";
+          version =
+            if (self ? rev)
+            then self.shortRev
+            else self.dirtyShortRev;
+          nativeBuildInputs = [poetry-core];
+          propagatedBuildInputs = [ poetry-core jira jinja2 python-frontmatter jira2markdown ];
+
+          meta = {
+            description = "Sync Jira issues to Markdown files";
+            homepage = "https://github.com/abraverm/jirasync";
+          };
+
         };
         default = self.packages.${system}.jirasync;
       };
